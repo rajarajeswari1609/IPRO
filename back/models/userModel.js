e7b460fb82b5c89839484fb254fd50b4886db575
rajarajeswari1609 <rajarajeswari59218@gmail.com>
2025-03-05 09:28:36 +0530

    userModel.js

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   email: { type: String, unique: true },
   password: String,
-  isVerified: { type: Boolean, default: false } // Add this field
+  isVerified: { type: Boolean, default: false } 
 });
 
 module.exports = mongoose.model("User", userSchema);
